0d7ba71db05eb850b60fe0c1489a13b0c54f9b27
Sayantan Das <91059186+NooB-Coder-8861@users.noreply.github.com>
2023-09-28 16:23:44 +0530

    Update SumOfEvenNumbers.java

--- SumOfEvenNumbers.java
+++ SumOfEvenNumbers.java
@@ -2,7 +2,7 @@ public class SumOfEvenNumbers {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 25; i < 52; i++) {
-            if (i / 2 == 0) {
+            if (i % 2 == 0) {
                 sum = sum + i;
             }
         }
