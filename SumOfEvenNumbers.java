336edc534008f18f06ad400bd232fec22659e992
Sayantan Das <91059186+NooB-Coder-8861@users.noreply.github.com>
2023-09-28 18:50:19 +0530

    Update SumOfEvenNumbers.java

--- SumOfEvenNumbers.java
+++ SumOfEvenNumbers.java
@@ -3,13 +3,13 @@ Sayantan Das
 */
 public class SumOfEvenNumbers {
     public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 25; i < 52; i++) {
-            if (i % 2 == 0) {
-                sum = sum + i;
+        int sum = 0;                        //Took a veriable to store the sum of all even numbers.
+        for (int i = 25; i < 52; i++) {    //We will go through every integer form 25 to 51
+            if (i % 2 == 0) {               //Checks if the number is even or not
+                sum = sum + i;               //add up the number
             }
         }
 
-        System.out.println("Sum of all even numbers between 25 to 51 is : " + sum);
+        System.out.println("Sum of all even numbers between 25 to 51 is : " + sum); //Display the value
     }
 }
